7f38a57d00de465cbddae13ae8d4e0c898d6e160
arun <arun36824@gmail.com>
2022-05-14 22:40:33 +0530

    make it better

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import { NEWS_API_KEY } from "./config";
 
+const p = 'https://api.codetabs.com/v1/proxy?quest='
 export const getBitcoinArticles = async () => {
   const response = await fetch(
-    `https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
+    `${p}https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
   );
   const json = await response.json();
   return json;
@@ -10,7 +11,7 @@ export const getBitcoinArticles = async () => {
 
 export const getArticles = async topic => {
   const response = await fetch(
-    `https://newsapi.org/v2/everything?q=${topic}&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
+    `${p}https://newsapi.org/v2/everything?q=${topic}&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
   );
   const json = await response.json();
   return json;
